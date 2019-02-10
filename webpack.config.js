945f3da85eebedef2c4b6a93358b8575c5ddb429
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-02-10 21:15:55 +0100

    init project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports={
+    mode: "development",
+    entry:"./scripts/app.js",
+    watch: true,
+    output: {
+        filename: "../scripts/out.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader?url=false',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['env', {
+                                targets: {
+                                    browsers: [
+                                        'Chrome 49'
+                                    ]
+                                }
+                            }]
+                        ]
+                    }
+                }
+            }]
+    }
+ };
